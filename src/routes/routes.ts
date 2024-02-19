6d693cbf6b86d6323e452536088b9951bd9b96ad
felix mavila <felixmacarenco@gmail.com>
2024-02-19 18:12:33 +0000

    redeploy

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/Users/CreateUserController";
 import { AuthUserController } from "../controllers/Users/AuthUserController";
+import { SaveResponseController } from "../controllers/Response/SaveResponseController";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 const router = Router()
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session',new AuthUserController().handle)
+router.post('/response',isAuthenticated,new SaveResponseController().handle)
 
 export {router}

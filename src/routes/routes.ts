51746796582765195a9373fed719162ebc4c11eb
felix mavila <felixmacarenco@gmail.com>
2024-02-20 00:21:40 +0000

    feat: crating user update feature

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,6 +7,7 @@ import { RegisterRateLimit } from "../shared/limit/RegisterRateLimit";
 import { SaveUserResponseController } from "../controllers/Response/SaveUserResponseController";
 import { ListUserResponseController } from "../controllers/Response/ListUserResponseController";
 import { RemoveUserResponseController } from "../controllers/Response/RemoveUserResponseController";
+import { UpdateUserController } from "../controllers/User/UpdateUserController";
 const router = Router()
 
 router.post('/users',RegisterRateLimit, new CreateUserController().handle)
@@ -14,5 +15,6 @@ router.post('/session',Authlimiter,new AuthUserController().handle)
 router.post('/response',isAuthenticated,new SaveUserResponseController().hadle)
 router.get('/response',isAuthenticated,new ListUserResponseController().handle)
 router.delete('/response/:id',isAuthenticated, new RemoveUserResponseController().handle)
+router.put('/users/:id',isAuthenticated,new UpdateUserController().handle)
 
 export {router}

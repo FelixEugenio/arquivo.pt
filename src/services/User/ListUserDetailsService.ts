3a80f9c8be4ced0409cd8d347e27a4310a109305
felix mavila <felixmacarenco@gmail.com>
2024-02-21 19:28:38 +0000

    feat: list user deatils

--- src/services/User/ListUserDetailsService.ts
+++ src/services/User/ListUserDetailsService.ts
@@ -5,6 +5,11 @@ class ListUserDeatilsService{
         const user = await prismaClient.user.findFirst({
             where:{
                 id:user_id
+            },
+            select:{
+                id:true,
+                email:true,
+                name:true
             }
         })
 

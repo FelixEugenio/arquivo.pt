44583e9138430c7fde341460101c919653ed8329
felix mavila <felixmacarenco@gmail.com>
2024-03-05 14:21:15 +0000

    feat: refactoring send email

--- src/controllers/User/CreateUserController.ts
+++ src/controllers/User/CreateUserController.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config';
 import { Request,Response } from "express";
 import { CreateUserService } from "../../services/User/CreateUserService";
 import {z} from 'zod'
 import Queue from '../../lib/Queue'
 import Mail from "../../lib/Mail";
+import {MailerSend,EmailParams,Sender,Recipient} from 'mailersend'
+
+
+
+
+
 
 class CreateUserController{
     async handle(req:Request,res:Response){
@@ -24,12 +31,35 @@ class CreateUserController{
             password
         })
 
+        const mailerSend = new MailerSend({
+            apiKey: process.env.API_KEY,
+          });
+
+          const sentFrom = new Sender("ArquivoGPT<@ArquivoGPT@gmail.com>", user.name);
+
+          const recipients = [
+            new Recipient(user.email, user.name)
+          ];
+
+          const emailParams = new EmailParams()
+          .setFrom(sentFrom)
+          .setTo(recipients)
+          .setReplyTo(sentFrom)
+          .setSubject("Cadastro de Usuario")
+          .setHtml("<strong>Ola Bem Vindo ao ArquivoGPT</strong>")
+          .setText("Obrigado Por Fazer Parte desta Familia");
+
+          await mailerSend.email.send(emailParams);
+
+          /*
         await Mail.sendMail({
             from:'ArquivoGPT<@ArquivoGPT@gmail.com>',
             to:`${user.name} <${user.email}>`,
             subject:'Cadastro de Usuario',
             html:`Ola ${name}, bem vindo ao arquivoGPT`
         })
+
+        */
 
         return res.json(user)
 

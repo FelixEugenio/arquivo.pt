cdf5bcfca42d79221b7cbb1798ea7b521ce1a549
felix mavila <felixmacarenco@gmail.com>
2024-03-05 13:40:29 +0000

    feat: reaftoring send mail config

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { CreateUserController } from "../controllers/User/CreateUserController";
 import { AuthUserController } from "../controllers/User/AuthUserController";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
-import { Authlimiter } from "../shared/limit/AuthRateLimit";
-import { RegisterRateLimit } from "../shared/limit/RegisterRateLimit";
 import { SaveUserResponseController } from "../controllers/Response/SaveUserResponseController";
 import { ListUserResponseController } from "../controllers/Response/ListUserResponseController";
 import { RemoveUserResponseController } from "../controllers/Response/RemoveUserResponseController";
@@ -11,8 +9,8 @@ import { UpdateUserController } from "../controllers/User/UpdateUserController";
 import { ListUserDetailsController } from "../controllers/User/ListUserDetailsController";
 const router = Router()
 
-router.post('/users',RegisterRateLimit, new CreateUserController().handle)
-router.post('/session',Authlimiter,new AuthUserController().handle)
+router.post('/users', new CreateUserController().handle)
+router.post('/session',new AuthUserController().handle)
 router.post('/response',isAuthenticated,new SaveUserResponseController().hadle)
 router.get('/response',isAuthenticated,new ListUserResponseController().handle)
 router.delete('/response/:id',isAuthenticated, new RemoveUserResponseController().handle)
